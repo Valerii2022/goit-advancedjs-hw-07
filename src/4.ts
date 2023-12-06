349f702a53d85e786e96fbcbb6c016ab40d02aba
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2023-12-07 00:13:00 +0200

    done with tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,66 @@
+interface KeyInterface {
+  getSignature(): number;
+}
+
+interface PersonInterface {
+  getKey(): KeyInterface;
+}
+
+interface HouseInterface {
+  comeIn(person: Person): void;
+  openDoor(key: Key): void;
+}
+
+class Key implements KeyInterface {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person implements PersonInterface {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+class House implements HouseInterface {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+    this.door = false;
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) this.tenants.push(person);
+  }
+
+  openDoor(key: Key): void {}
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) this.door = true;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +69,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
